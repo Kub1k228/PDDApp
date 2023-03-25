ef0caf1dc91cfe846d4abb180911b74de119ab75
Luppov Ivan <ivan.luppov@gmail.com>
2023-03-25 14:44:00 +0300

    LogCat

--- app/src/main/java/com/example/pddapp/MainActivity.java
+++ app/src/main/java/com/example/pddapp/MainActivity.java
@@ -3,20 +3,37 @@ package com.example.pddapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyApp";
+    TextView textView;
+    ImageView imageView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+        textView.setText(R.string.var1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Это мое сообщение для записи в журнале");
+            }
+        });
+
+    }
+    private void initUI (){
+        textView = findViewById(R.id.textView4);
+        button = findViewById(R.id.button11);
+
     }
 
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
 
-        }
-    };
 }
