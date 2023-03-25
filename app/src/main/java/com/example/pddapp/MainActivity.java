8385d77c84432b8ee73b5fcbd6f85292dd7b2271
Kub1k228 <76582278+Kub1k228@users.noreply.github.com>
2023-03-25 11:37:38 +0300

    new strings and colors

--- app/src/main/java/com/example/pddapp/MainActivity.java
+++ app/src/main/java/com/example/pddapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pddapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 }
